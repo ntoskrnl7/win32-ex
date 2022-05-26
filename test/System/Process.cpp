88a8fdeaee9cd29893367f60bc5ad22f470b21fd
Jung-Kwang Lee <ntoskrnl7@gmail.com>
2022-05-26 09:37:14 +0900

    Fix test code

--- test/System/Process.cpp
+++ test/System/Process.cpp
@@ -197,8 +197,8 @@ TEST(ProcessTest, StdIn)
 {
     Win32Ex::System::UserAccountProcess process("cmd /c more");
     process.RunAsync();
-    process.StdIn() << "test 1\n";
-    process.StdIn() << "test 2\ntest 3\n";
+    process.StdIn() << "test 1\r\n";
+    process.StdIn() << "test 2\r\ntest 3\r\n";
     process.StdIn().Close();
     EXPECT_STREQ(process.StdOut().ReadAll().c_str(), "test 1\r\ntest 2\r\ntest 3\r\n\r\n");
 }
